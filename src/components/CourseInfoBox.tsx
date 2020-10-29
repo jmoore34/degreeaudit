08fcb42122b98cac9cc84077afe1b745903ae0c1
Christopher Sweetman <chrisjdsweetman@gmail.com>
2020-10-28 22:33:56 -0500

    Added prototype functionality to the button Changes made: onButton has styling done. Utilizes useState to keep track of which button is selected. Started working on div sibling to the iframe to cover or filter gray/text area.
    Security Concerns: N/A

--- src/components/CourseInfoBox.tsx
+++ src/components/CourseInfoBox.tsx
@@ -17,10 +17,13 @@ const InfoBox = styled.div`
 const SemesterButton = styled.div <{ color: string, selected: boolean, doubleWidth: boolean }>`
     background-color: ${props => props.selected ? Color(props.color).lighten(.3).toString() : Color(props.color).lighten(.96).toString()};
     border: 2px solid ${props => props.selected ? Color(props.color).lighten(.1).toString() : Color(props.color).lighten(.36).toString()};
-    box-shadow: 4px 4px 4px 1px lightgray;
+    transition: all .15s;
+    font-weight: ${props => props.selected ? "bold" : "normal"};
+    box-shadow: ${props => props.selected ? "6px 6px 6px 1px lightgray" : "4px 4px 4px 1px lightgray"};
+    transform: ${props => props.selected ? "scale(1.02)" : ""};
     cursor: pointer;
     grid-column: span ${props => props.doubleWidth ? 3 : 1};
-    padding: 0.3rem;
+    padding: .3rem;
     margin: 0.2rem;
 `
 
@@ -29,18 +32,55 @@ const ButtonContainer = styled.div <{}>`
     grid-template-columns: repeat(6, 1fr);
 `
 
+const CourseFrame = styled.iframe <{}>`
+    margin-top: -90px;
+    pointer-events: none;
+    width: 639px;
+    height: 500px;
+    //clip:rect(100px,700px,620px,00px);
+    left: 0px;
+    position: relative;
+    z-index: -1000;
+`
+const IFrameWrapper = styled.div <{}>`
+    overflow: hidden;
+    position: relative;
+    border: 1px solid red;
+    top:-10px;
+    height: 390px;
+    //background-color: blue;
+`
+const SiblingDiv = styled.div <{}>`
+    background-color: blue;
+    position: absolute;
+    width: 70%;
+    height: 390px;
+`
 
-export const CourseInfoBox: FunctionComponent<{}> = () => {
+export const CourseInfoBox: FunctionComponent<{}> = (props) => {
     const [semester, setSemester] = useState("");
+
 
     return <>
         <InfoBox>
-            <h1>Course Title</h1>
-            <p>Course Description</p>
+            {/*<IFrameWrapper>
+                <CourseFrame src="https://catalog.utdallas.edu/2020/undergraduate/courses/cs2305" height="400" width="600" scrolling="no"></CourseFrame>
+            </IFrameWrapper>*/}
+            <IFrameWrapper>
+                <CourseFrame src="https://catalog.utdallas.edu/2020/undergraduate/courses/cs2305" height="400" width="600" scrolling="no"></CourseFrame>
+            </IFrameWrapper>
             <ButtonContainer>
                 {["Previous Semesters", "Current Semester", "Spring 21", "Sum. 21", "Fall 21", "Spring 22", "Sum. 22", "Fall 22", "Spring 23", "Sum. 23", "Fall 23",
-                    "Spring 24", "Sum. 24", "Fall 24"].map((sem, index, arr) => <SemesterButton color={`hsl(${(index / arr.length) * 360},100%,50%)`}
-                        selected={false} doubleWidth={sem.includes("Sem") ? true : false}>{sem}</SemesterButton>)}
+                    "Spring 24", "Sum. 24", "Fall 24"].map((sem, index, arr) =>
+                        <SemesterButton
+                            color={`hsl(${(index / arr.length) * 360},100%,50%)`}
+                            selected={semester === sem}
+                            doubleWidth={sem.includes("Sem") ? true : false}
+                            onClick={() => {
+                                setSemester(sem);
+                            }}>
+                            {sem}
+                        </SemesterButton>)}
             </ButtonContainer>
         </InfoBox>
     </>
