4c2936f50d9c0644944a7c28f996e9c7b611fca0
Jonathan Moore <jtm170330@utdallas.edu>
2020-12-03 14:44:53 -0600

    Add setup.py and requirements.txt

---
+++ FlaskIntro/setup.py
@@ -0,0 +1,23 @@
+import os
+from passlib.hash import sha256_crypt
+
+PASSWORD_FILE = '../PASSWORD_HASH_DO_NOT_COMMIT'
+
+def change_password(newPassword):
+    with open(PASSWORD_FILE, 'w') as f:
+        f.write(sha256_crypt.hash(newPassword))
+
+# uncomment to set password
+#change_password("password goes here")
+
+print("Installing dependencies...")
+print(" Running: pip3 install -r requirements.txt")
+os.system("pip3 install -r requirements.txt")
+
+print("Enter the advisor password:")
+password = input()
+
+change_password(password)
+
+print(f"Password changed to '{password}'. Advisor view (keep secret - do not share with students) available at https://ecsadvising.utdallas.edu/degreeaudit/#{password}")
+print(f"Student view accessible at https://ecsadvising.utdallas.edu/degreeaudit/")
